b5eb2df8a31c3fd1405ee8995d79e62c6ac5023b
ADSF <aaron881015@hotmail.com.tw>
2022-02-11 23:34:46 +0800

    login home page change navbar and page seperately

--- lib/home.dart
+++ lib/home.dart
@@ -2,30 +2,61 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'main.dart';
+import 'page/loginhome.dart';
+import 'page/statistics.dart';
+import 'page/store.dart';
 
-class LoginHomePage extends StatelessWidget {
+class LoginHomePage extends StatefulWidget {
+  @override
+  State<LoginHomePage> createState() => _LoginHomePageState();
+}
+
+class _LoginHomePageState extends State<LoginHomePage> {
   final user = FirebaseAuth.instance.currentUser!;
+  int pageIndex = 0;
+
+  final screen = [
+    HomeContentPage(),
+    StatisticsPage(),
+    StorePage(),
+  ];
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(12),
-          child: Column(
-            children: <Widget>[
-              Text('Welcome ${user.email}'),
-              ElevatedButton(
-                child: Text('Logout'),
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.pop(context);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-              )
+        bottomNavigationBar: NavigationBarTheme(
+          data: NavigationBarThemeData(
+            indicatorColor: Colors.blue.shade100,
+            labelTextStyle: MaterialStateProperty.all(
+              TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          child: NavigationBar(
+            selectedIndex: pageIndex,
+            onDestinationSelected: (index) => setState(() => pageIndex = index),
+            labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            animationDuration: Duration(seconds: 1),
+            destinations: [
+              NavigationDestination(
+                icon: Icon(Icons.home_outlined),
+                selectedIcon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.assessment_outlined),
+                selectedIcon: Icon(Icons.assessment),
+                label: 'Statistics',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.store_outlined),
+                selectedIcon: Icon(Icons.store),
+                label: 'Store',
+              ),
             ],
           ),
         ),
+        body: screen[pageIndex],
       );
 }
---
+++ lib/page/loginhome.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeContentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Store',
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+    );
+  }
+}

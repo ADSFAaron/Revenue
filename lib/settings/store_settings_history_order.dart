542925ba60482c99d9ff928f627c8f3a84063fae
ADSF <aaron881015@hotmail.com.tw>
2022-02-28 23:08:45 +0800

    fix history page

--- lib/settings/store_settings_history_order.dart
+++ lib/settings/store_settings_history_order.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StoreHistoryOrder extends StatelessWidget {
+  String storeID;
+  StoreHistoryOrder(this.storeID, {Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

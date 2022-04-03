6bb061efe12ff387572de093930c037e020a20bc
ADSF <aaron881015@hotmail.com.tw>
2022-04-03 20:53:34 +0800

    order page finish~

--- lib/page/addorder.dart
+++ lib/page/addorder.dart
@@ -27,9 +27,16 @@ class _AddOrderState extends State<AddOrder> {
         lastDate: DateTime(DateTime.now().year + 5));
 
     if (date != null) {
-      setState(() {
-        pickDate = date;
-      });
+      TimeOfDay pickTime = TimeOfDay.now();
+      final tmp = await showTimePicker(context: context, initialTime: pickTime);
+
+      if (tmp != null) {
+        setState(() {
+          pickDate = new DateTime(
+              date.year, date.month, date.day, tmp.hour, tmp.minute);
+        });
+      }
+      print(pickDate);
     }
   }
 
@@ -68,7 +75,7 @@ class _AddOrderState extends State<AddOrder> {
               menuList[i] = menuList[i] as Map<String, dynamic>;
               menuList[i]['amount'] = 0;
             }
-
+            print('load data from firestore');
             print(menuList);
           }
 
@@ -83,7 +90,8 @@ class _AddOrderState extends State<AddOrder> {
                         horizontal: 16.0, vertical: 8.0),
                     child: ListTile(
                       title: Text(
-                          "日期 :  ${pickDate.year}  / ${pickDate.month}  / ${pickDate.day}"),
+                          "日期 :  ${pickDate.year}  / ${pickDate.month}  / ${pickDate.day}" +
+                              "  ${pickDate.hour.toString().padLeft(2, '0')}:${pickDate.minute.toString().padLeft(2, '0')}"),
                       trailing: Icon(
                         Icons.calendar_today,
                         color: Colors.grey[700],
@@ -126,6 +134,9 @@ class _AddOrderState extends State<AddOrder> {
                                         if (menuList[index]['amount'] > 0) {
                                           setState(() {
                                             menuList[index]['amount']--;
+                                            totalCount -= int.parse(
+                                                menuList[index]['price']
+                                                    .toString());
                                           });
                                         }
                                       },
@@ -146,6 +157,9 @@ class _AddOrderState extends State<AddOrder> {
                                       onPressed: () {
                                         setState(() {
                                           menuList[index]['amount']++;
+                                          totalCount += int.parse(
+                                              menuList[index]['price']
+                                                  .toString());
                                         });
                                       },
                                       icon: const Icon(Icons.add),
@@ -244,5 +258,84 @@ class _AddOrderState extends State<AddOrder> {
     );
   }
 
-  void updateOrderToFirebase() {}
+  void updateOrderToFirebase() {
+    List<dynamic> orderList = [];
+    List<dynamic> origin = [];
+    int total = 0;
+
+    // Filter current order which is zero
+    for (int i = 0; i < menuList.length; i++) {
+      if (menuList[i]['amount'] != 0) {
+        orderList.add(menuList[i]);
+        total += (int.parse(menuList[i]['price'].toString()) *
+            int.parse(menuList[i]['amount'].toString()));
+      }
+    }
+
+    print(orderList);
+    print(menuList);
+
+    // Check if it has order, not to create null order
+    if (orderList.isEmpty) {
+      const snackBar = SnackBar(
+        duration: Duration(milliseconds: 1000),
+        content: Text('No item add in order!'),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      return;
+    }
+
+    // Check if Document is exist
+    CollectionReference order =
+        FirebaseFirestore.instance.collection('tmporder');
+
+    DocumentReference document = order.doc(widget.storeId);
+    document.get().then((DocumentSnapshot value) {
+      print('get data');
+      print(value.data());
+      print(value.data().runtimeType);
+      print('total count:');
+      print(total);
+
+      if (value.exists) {
+        print('update');
+        // Update
+        Map<String, dynamic> allData = value.data() as Map<String, dynamic>;
+        allData['orders'].add({
+          "details": orderList,
+          "time": pickDate,
+          "no": stores['orderIndex'],
+          "total": total
+        });
+        document.update({
+          'orders': allData['orders'],
+        });
+      } else {
+        print('create');
+        // Create
+        List<dynamic> allOrders = [];
+        allOrders.add({
+          "details": orderList,
+          "time": pickDate,
+          "no": stores['orderIndex'],
+          "total": total
+        });
+        document.set({
+          'orders': allOrders,
+        });
+      }
+    });
+    // document.update({"orders": menuList}).then((value) {
+    //   print("successful add");
+    // }).catchError((error) {
+    //   print(error);
+    // });
+
+    setState(() {
+      totalCount = 0;
+      menuList = [];
+    });
+  }
 }

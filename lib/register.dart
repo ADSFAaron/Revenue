b41cf52cd0e9a37b2ed70002b5693f3be386bcff
ADSFAaron <aaron881015@hotmail.com.tw>
2022-05-30 22:32:49 +0800

    register page can add user to firebase

--- lib/register.dart
+++ lib/register.dart
@@ -1,11 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uuid/uuid.dart';
 
 import 'login.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  late TextEditingController emailController,
+      passwordController,
+      rePasswordController,
+      storeIDController;
+  bool _validate = false, showPassword = false;
+  String passwordErrorMsg = "",
+      mailErrorMsg = "",
+      storeIDMsg = "",
+      rePasswordErrorMsg = "";
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    rePasswordController = TextEditingController();
+    storeIDController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    rePasswordController.dispose();
+    storeIDController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,11 +88,56 @@ class RegisterPage extends StatelessWidget {
                           EdgeInsets.symmetric(vertical: 20, horizontal: 40),
                       child: Column(
                         children: <Widget>[
-                          makeInput(label: "Email"),
-                          makeInput(label: "Password", obscureText: true),
                           makeInput(
-                              label: "Confirm Password", obscureText: true),
-                          makeInput(label: "StoreID"),
+                              label: "Email",
+                              Controller: emailController,
+                              errorDescription: mailErrorMsg),
+                          makeInput(
+                              label: "Password",
+                              Controller: passwordController,
+                              obscureText: showPassword,
+                              errorDescription: passwordErrorMsg),
+                          makeInput(
+                              label: "Confirm Password",
+                              Controller: rePasswordController,
+                              errorDescription: rePasswordErrorMsg,
+                              obscureText: false),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "StoreID",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.black87),
+                              ),
+                              SizedBox(height: 5),
+                              TextField(
+                                controller: storeIDController,
+                                decoration: InputDecoration(
+                                  errorText:
+                                      storeIDMsg == "" ? null : storeIDMsg,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      vertical: 0, horizontal: 10),
+                                  border: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.grey)),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.grey)),
+                                  suffixIcon: TextButton(
+                                      onPressed: () {
+                                        var uuid = Uuid();
+                                        String u = uuid.v4();
+                                        storeIDController.text = u;
+                                      },
+                                      child: Text("Generate")),
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                            ],
+                          ),
                         ],
                       ),
                     ),
@@ -74,10 +154,71 @@ class RegisterPage extends StatelessWidget {
                         child: MaterialButton(
                           height: 60,
                           minWidth: MediaQuery.of(context).size.width,
-                          onPressed: () {
-                            // Determine the mail is exist or not
-                            // If exist, show error message
-                            // Check Store ID is exist or new generated
+                          onPressed: () async {
+                            print('Email: $emailController.text');
+
+                            if (emailController.text.isEmpty) {
+                              setState(() {
+                                mailErrorMsg = 'Enter the email';
+                              });
+                              return;
+                            } else {
+                              setState(() {
+                                mailErrorMsg = '';
+                              });
+                            }
+                            if (passwordController.text.isEmpty) {
+                              setState(() {
+                                passwordErrorMsg = 'Enter the password';
+                              });
+                              return;
+                            } else {
+                              setState(() {
+                                passwordErrorMsg = '';
+                              });
+                            }
+
+                            if (storeIDController.text.isEmpty) {
+                              setState(() {
+                                storeIDMsg = 'Enter the store ID';
+                              });
+                              return;
+                            } else {
+                              setState(() {
+                                storeIDMsg = '';
+                              });
+                            }
+
+                            if (passwordController.text ==
+                                rePasswordController.text) {
+                              try {
+                                UserCredential userCredential =
+                                    await FirebaseAuth.instance
+                                        .createUserWithEmailAndPassword(
+                                            email: emailController.text,
+                                            password: passwordController.text);
+                              } on FirebaseAuthException catch (e) {
+                                if (e.code == 'weak-password') {
+                                  print('The password provided is too weak.');
+                                  setState(() {
+                                    passwordErrorMsg =
+                                        'The password provided is too weak.';
+                                  });
+                                } else if (e.code == 'email-already-in-use') {
+                                  print(
+                                      'The account already exists for that email.');
+                                  mailErrorMsg =
+                                      'The account already exists for that email.';
+                                }
+                              } catch (e) {
+                                print(e);
+                              }
+                            } else {
+                              setState(() {
+                                rePasswordErrorMsg =
+                                    'Confirm Password no match';
+                              });
+                            }
                           },
                           color: Colors.greenAccent,
                           elevation: 0,
@@ -124,7 +265,7 @@ class RegisterPage extends StatelessWidget {
     );
   }
 
-  Widget makeInput({label, obscureText = false}) {
+  Widget makeInput({label, Controller, obscureText = true, errorDescription}) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -135,15 +276,26 @@ class RegisterPage extends StatelessWidget {
         ),
         SizedBox(height: 5),
         TextField(
-          obscureText: obscureText,
+          controller: Controller,
+          obscureText: !obscureText,
           decoration: InputDecoration(
+            errorText: errorDescription == "" ? null : errorDescription,
             contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
             border:
                 OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
             enabledBorder:
                 OutlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-            suffixIcon: label == "StoreID"
-                ? TextButton(onPressed: () {}, child: Text("Generate"))
+            suffixIcon: label == "Password"
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },
+                    icon: showPassword
+                        ? Icon(Icons.visibility)
+                        : Icon(Icons.visibility_off),
+                  )
                 : null,
           ),
         ),

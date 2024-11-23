945fc65083093b8cd345100b30af9a5cacec0631
ADSFAaron <aaron881015@hotmail.com.tw>
2024-11-24 00:16:13 +0800

    * Fix firebase connection on ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'firebase_options.dart';
 
 import 'animation/FadeAnimation.dart';
+import 'firebase_options.dart';
 import 'home.dart';
 import 'login.dart';
 import 'register.dart';
@@ -22,8 +22,10 @@ final navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
